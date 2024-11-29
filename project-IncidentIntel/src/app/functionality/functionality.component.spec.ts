7b770b82560ddb000e22e333a67caafc9e07b5e7
sunilsahu-gep <110446184+sunilsahu-gep@users.noreply.github.com>
2024-11-29 15:02:13 +0530

    functionality componet

---
+++ project-IncidentIntel/src/app/functionality/functionality.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FunctionalityComponent } from './functionality.component';
+
+describe('FunctionalityComponent', () => {
+  let component: FunctionalityComponent;
+  let fixture: ComponentFixture<FunctionalityComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ FunctionalityComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FunctionalityComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ project-IncidentIntel/src/app/functionality/functionality.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-functionality',
+  templateUrl: './functionality.component.html',
+  styleUrls: ['./functionality.component.css']
+})
+export class FunctionalityComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
