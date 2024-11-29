e6b07905876efc35884d50be583a4fc1698ffcbc
JJoyalHaris-GEP <110533050+JJoyalHaris-GEP@users.noreply.github.com>
2024-11-29 15:13:53 +0530

    landingComponent

--- project-IncidentIntel/src/app/app.module.ts
+++ project-IncidentIntel/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { FunctionalityComponent } from './functionality/functionality.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    FunctionalityComponent
+    FunctionalityComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
